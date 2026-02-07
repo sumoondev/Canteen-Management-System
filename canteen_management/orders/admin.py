b4a6323662282af7c715fd7daf3a7617e0189da0
Subham Parajuli <suvamparajuli222@gmail.com>
2026-02-07 17:38:13 +0545

    orders database completed next:payments

--- canteen_management/orders/admin.py
+++ canteen_management/orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order, OrderItem
 
 # Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model=OrderItem
+    extra=0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'total_amount', 'order_date')
+    inlines = [OrderItemInline]  
